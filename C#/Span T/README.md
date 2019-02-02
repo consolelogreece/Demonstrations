8c09cbfa313238b0d18c48276ae4dee89d81462b
reece <consolelogreece@gmail.com>
2019-02-02 21:29:25 +0000

    added arbitrary pointers proof and some info docs

---
+++ C#/Span T/README.md
@@ -0,0 +1,3 @@
+This is a explanation of C# 7.2's span<T>, it's pros, cons and some demonstrations.
+
+Read "Span T info.txt" to get started.
---
+++ C#/Span T/Span T/ArbitraryPointers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Span_T
+{
+    // Span can refer to any arbitrary block of contiguous memory. 
+    // Consider this memory from the native heap
+    public static class ArbitraryPointers
+    {
+        public static void ArbitraryPointersDemo()
+        {
+            IntPtr pointer = Marshal.AllocHGlobal(5);
+
+            Span<int> numbers;
+            unsafe
+            {
+                // This is the magic. 
+                // This unmanaged block of contiguous memory, usually unsafe, is now wrapped by the Span class and can be used in a safe context.
+               numbers = new Span<int>((int*)pointer, 5);
+            }
+
+            // Can access memory outside of unsafe block.
+            numbers[6] = 12; // Exception thrown, as if managed array.
+        }
+
+    }
+}
