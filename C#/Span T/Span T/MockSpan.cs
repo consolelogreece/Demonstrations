867b3e18a49b0b7ae08e3ec52648093aeebeecd7
reece <consolelogreece@gmail.com>
2019-02-03 17:00:43 +0000

    updated examples and info sheet

---
+++ C#/Span T/Span T/MockSpan.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Span_T
+{
+    // In order to implement your own version of Span<T>, you'd need a reference field so you could modify the region of memory directly,
+    // and not just modify a copy of it. Fields of type 'ref' are illegal in C#.
+    public class MockSpan<T>
+    { 
+        private int _length;
+
+        private ref T _pointer; // Does not compile.
+
+        public MockSpan(T[] arr)
+        {
+            _pointer = ref arr[0];
+
+            _length = arr.Length;
+        } 
+
+        public MockSpan(T[] arr, int length)
+        {
+            _pointer = ref arr[0];
+
+            _length = length;
+        }
+
+        // Span sets pointer to the nth element, as determined by start.
+        public MockSpan(T[] arr, int start, int length)
+        {
+            _pointer = ref arr[start];
+
+            _length = length;
+        }
+
+        // ref T return indexer overload.
+        public ref T this[int index]
+        {
+            get
+            {
+                // logic goes here.
+            }
+        }
+    }
+}
--- C#/Span T/Span T/Structure.cs
+++ C#/Span T/Span T/Structure.cs
@@ -7,19 +7,16 @@ namespace Span_T
 {
     public static class Structure
     {
-        // TODO : LOOK DEEPER INTO WHETHER SPAN EVEN HAS OFFSETS. https://adamsitnik.com/Span/
         public static void StructureDemo()
         {
-            // Pointer = Numbers.
-            // Offset = 0.
+            // Pointer = the first number, as no offset is provided on construction.
             // length = 9.
             int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             Span<int> NumberSpan = Numbers;
 
 
-            // Pointer = Numbers,
-            // Offset = 3.
-            // Length = 2.
+            // Offset of 3 provided, meaning the pointer points to the third element.
+            // Length = 2, so holds positions 3 & 4;
             Span<int> NumberSegment = NumberSpan.Slice(3, 2);
 
         }
