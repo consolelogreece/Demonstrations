b37512105f4db6190128a1251c2ed45f7cab3c46
reece <consolelogreece@gmail.com>
2019-02-03 17:22:49 +0000

    cleanup

--- C#/Span T/Span T/Program.cs
+++ C#/Span T/Span T/Program.cs
@@ -5,23 +5,5 @@ namespace Span_T
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            UnsafeStackData();
-
-            Console.Read();
-        }
-
-
-
-        static unsafe void UnsafeStackData()
-        {
-            int* numbers = stackalloc int[5];
-            numbers[1] = 5;
-            numbers[2] = 50;
-            numbers[6] = 44; // No Exception Thrown.  
-
-            var x = numbers[7];
-        }
     }
 }
