ab09150991fa63794b678ee5ce5f245c52b484a6
reece <consolelogreece@gmail.com>
2019-03-23 19:07:42 +0000

    added websockets demo using socket.io

---
+++ JavaScript/Web Sockets/server/server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('disconnect', () => console.log("a user disconnected"))
+    socket.on("message", (x) => {
+        io.emit("message", {
+            message: x,
+            timestamp: Date.now()
+        })
+    });
+    console.log('a user connected');
+});
+
+http.listen(3030, function(){
+  console.log('listening on *:3030');
+});
